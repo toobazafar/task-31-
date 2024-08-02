5e0b664b26cb6d9bd183a4b5abad42cda585627c
TOOBA ZAFAR <160070906+toobazafar@users.noreply.github.com>
2024-08-02 22:28:41 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,10 @@
+let users : string [] = ['sana','babar','saba','taha','admin']
+if(users.length === 0){
+console.log("We need to find some users! ")
+}
+// • Remove all of the usernames from your array,
+// and make sure the correct message is printed
+else{
+users = [];
+console.log("All user have been removed " + users.length)
+}
